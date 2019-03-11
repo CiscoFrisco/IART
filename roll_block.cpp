3ccbcb2c8d165b6934084ce39e46366ed1f56c56
Rekicho <up201604145@fe.up.pt>
2019-03-11 16:43:29 +0000

    Better representation

--- roll_block.cpp
+++ roll_block.cpp
@@ -1,128 +1,110 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <memory>
 #include <chrono>
-
-#define NOTHING ' '
-#define STANDING '|'
-#define LAYING '-'
-#define GROUND 'g'
-#define HOLE '_'
-#define FINISH 'F'
 
 using namespace std;
 using namespace std::chrono;
 
-int nodes = 0;
+struct Position
+{
+	int i;
+	int j;
+
+	bool operator==(const Position &other) const
+	{
+		return i == other.i && j == other.j;
+	}
+
+	bool operator<(const Position &other) const
+	{
+		if(i < other.i)
+			return true;
+
+		if(i > other.i)
+			return false;
+
+		return j < other.j;
+	}
+};
 
 struct State
 {
-	vector<vector<char>> map;
-	int lastOp = -1;
+	Position pos1;
+	Position pos2;
+	shared_ptr<State> parent;
+
+	bool operator==(const State &other) const
+	{
+		return pos1 == other.pos1;
+	}
+
+	bool operator<(const State &other) const
+	{
+		return pos1 < other.pos1;
+	}
 };
 
-void findBlock(vector<vector<char>> &map, int &block_i, int &block_j)
+bool operator==( const shared_ptr<State>& lhs, const shared_ptr<State>& rhs )
 {
+	if(lhs == nullptr || rhs == nullptr)
+		return false;
+
+	return (*lhs) == (*rhs);
+}
+
+bool operator<( const shared_ptr<State>& lhs, const shared_ptr<State>& rhs )
+{
+	return (*lhs) < (*rhs);
+}
+
+int nodes = 0;
+vector<vector<char>> map = {
+	{'g', 'g', 'g', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{'g', '|', 'g', 'g', 'g', 'g', ' ', ' ', ' ', ' '},
+	{'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', ' '},
+	{' ', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g'},
+	{' ', ' ', ' ', ' ', ' ', 'g', 'g', '_', 'g', 'g'},
+	{' ', ' ', ' ', ' ', ' ', ' ', 'g', 'g', 'g', ' '}};
+
+set<Position> possible;
+Position goal;
+
+shared_ptr<State> analyzeMap()
+{
+	shared_ptr<State> start(new State);
+	start->parent = nullptr;
+	Position pos;
+
 	for (int i = 0; i < map.size(); ++i)
 		for (int j = 0; j < map[i].size(); ++j)
-			if (map[i][j] == STANDING)
+			switch (map[i][j])
 			{
-				block_i = i;
-				block_j = j;
-				return;
+			case 'g':
+				pos.i = i;
+				pos.j = j;
+				possible.insert(pos);
+				break;
+			case '|':
+				start->pos1.i = i;
+				start->pos1.j = j;
+				pos.i = i;
+				pos.j = j;
+				possible.insert(pos);
+				break;
+			case '_':
+				goal.i = i;
+				goal.j = j;
+				break;
+			default:
+				break;
 			}
+
+	return start;
 }
 
-bool up(vector<vector<char>> &map)
-{
-	int block_i, block_j;
-	findBlock(map, block_i, block_j);
-
-	if (block_i <= 0 || map[block_i - 1][block_j] == NOTHING)
-		return false;
-
-	map[block_i][block_j] = GROUND;
-
-	if (map[block_i - 1][block_j] == HOLE)
-		map[block_i - 1][block_j] = FINISH;
-
-	else
-		map[block_i - 1][block_j] = STANDING;
-
-	return true;
-}
-
-bool left(vector<vector<char>> &map)
-{
-	int block_i, block_j;
-	findBlock(map, block_i, block_j);
-
-	if (block_j <= 0 || map[block_i][block_j - 1] == NOTHING)
-		return false;
-
-	map[block_i][block_j] = GROUND;
-
-	if (map[block_i][block_j - 1] == HOLE)
-		map[block_i][block_j - 1] = FINISH;
-
-	else
-		map[block_i][block_j - 1] = STANDING;
-
-	return true;
-}
-
-bool right(vector<vector<char>> &map)
-{
-	int block_i, block_j;
-	findBlock(map, block_i, block_j);
-
-	if (block_j >= map[block_i].size() - 1 || map[block_i][block_j + 1] == NOTHING)
-		return false;
-
-	map[block_i][block_j] = GROUND;
-
-	if (map[block_i][block_j + 1] == HOLE)
-		map[block_i][block_j + 1] = FINISH;
-
-	else
-		map[block_i][block_j + 1] = STANDING;
-
-	return true;
-}
-
-bool down(vector<vector<char>> &map)
-{
-	int block_i, block_j;
-	findBlock(map, block_i, block_j);
-
-	if (block_i >= map.size() - 1 || map[block_i + 1][block_j] == NOTHING)
-		return false;
-
-	map[block_i][block_j] = GROUND;
-
-	if (map[block_i + 1][block_j] == HOLE)
-		map[block_i + 1][block_j] = FINISH;
-
-	else
-		map[block_i + 1][block_j] = STANDING;
-
-	return true;
-}
-
-typedef bool (*Operators)(vector<vector<char>> &map);
-
-vector<Operators> operators = {up, left, right, down};
-
-bool checkDone(const vector<vector<char>> &map)
-{
-	for (int i = 0; i < map.size(); ++i)
-		for (int j = 0; j < map[i].size(); ++j)
-			if (map[i][j] == FINISH)
-				return true;
-
-	return false;
-}
-
-void displayMap(const vector<vector<char>> &map)
+void displayMap(const shared_ptr<State> &state)
 {
 	cout << endl;
 	cout << " ";
@@ -155,7 +137,16 @@ void displayMap(const vector<vector<char>> &map)
 				if (j % 2 == 0)
 					cout << "|";
 				else
-					cout << ' ' << map[i / 2][j / 2] << ' ';
+				{
+					if (state->pos1.i == i / 2 && state->pos1.j == j / 2)
+						cout << " | ";
+
+					else if (map[i / 2][j / 2] == '|')
+						cout << " g ";
+
+					else
+						cout << ' ' << map[i / 2][j / 2] << ' ';
+				}
 			}
 		}
 		if (i % 2 != 0)
@@ -173,13 +164,95 @@ void displayMap(const vector<vector<char>> &map)
 	cout << endl;
 }
 
-State breadthFirstSearch(vector<State> &states)
+bool up(shared_ptr<State> &state)
 {
-	State res;
+	Position pos;
+	pos.i = state->pos1.i - 1;
+	pos.j = state->pos1.j;
+
+	if (goal == pos || possible.find(pos) != possible.end())
+	{
+		state->pos1 = pos;
+		return true;
+	}
+
+	return false;
+}
+
+bool left(shared_ptr<State> &state)
+{
+	Position pos;
+	pos.i = state->pos1.i;
+	pos.j = state->pos1.j - 1;
+
+	if (goal == pos || possible.find(pos) != possible.end())
+	{
+		state->pos1 = pos;
+		return true;
+	}
+
+	return false;
+}
+
+bool right(shared_ptr<State> &state)
+{
+	Position pos;
+	pos.i = state->pos1.i;
+	pos.j = state->pos1.j + 1;
+
+	if (goal == pos || possible.find(pos) != possible.end())
+	{
+		state->pos1 = pos;
+		return true;
+	}
+
+	return false;
+}
+
+bool down(shared_ptr<State> &state)
+{
+	Position pos;
+	pos.i = state->pos1.i + 1;
+	pos.j = state->pos1.j;
+
+	if (goal == pos || possible.find(pos) != possible.end())
+	{
+		state->pos1 = pos;
+		return true;
+	}
+
+	return false;
+}
+
+typedef bool (*Operators)(shared_ptr<State> &state);
+
+vector<Operators> operators = {up, left, right, down};
+
+bool checkDone(const shared_ptr<State> &state)
+{
+	return state->pos1 == goal;
+}
+
+bool findDuplicate(shared_ptr<State> parent, shared_ptr<State> child)
+{
+	while(parent != nullptr)
+	{
+		if(parent == child)
+			return true;
+
+		parent = parent->parent;
+	}
+
+	return false;
+}
+
+shared_ptr<State> breadthFirstSearch(vector<shared_ptr<State>> &states)
+{
+	shared_ptr<State> res;
 
 	while (states.size() != 0)
 	{
-		if (checkDone(states[0].map))
+		if (checkDone(states[0]))
 		{
 			res = states[0];
 			break;
@@ -187,20 +260,46 @@ State breadthFirstSearch(vector<State> &states)
 
 		for (int i = 0; i < operators.size(); ++i)
 		{
-			if(states[0].lastOp == 0 && i == 3
-			|| states[0].lastOp == 3 && i == 0
-			|| states[0].lastOp == 2 && i == 1
-			|| states[0].lastOp == 1 && i == 2)
-				continue;
-				
-			vector<vector<char>> map_temp = states[0].map;
+			shared_ptr<State> new_state(new State);
+			new_state->pos1 = states[0]->pos1;
+			new_state->parent = states[0];
+			
 
-			if (operators[i](map_temp))
+			if (operators[i](new_state) && !findDuplicate(states[0],new_state))
 			{
-				State new_state;
-				new_state.map = map_temp;
-				new_state.lastOp = i;
 				states.push_back(new_state);
+				++nodes;
+			}
+		}
+
+		states.erase(states.begin());
+	}
+
+	return res;
+}
+
+shared_ptr<State> depthFirstSearch(vector<shared_ptr<State>> &states)
+{
+	shared_ptr<State> res;
+
+	while (states.size() != 0)
+	{
+		if (checkDone(states[0]))
+		{
+			res = states[0];
+			break;
+		}
+
+		for (int i = 0; i < operators.size(); ++i)
+		{
+			shared_ptr<State> new_state(new State);
+			new_state->pos1 = states[0]->pos1;
+			new_state->parent = states[0];
+			
+
+			if (operators[i](new_state) && !findDuplicate(states[0],new_state))
+			{
+				states.insert(states.begin()+1, new_state);
 				++nodes;
 			}
 		}
@@ -213,30 +312,23 @@ State breadthFirstSearch(vector<State> &states)
 
 int main()
 {
-	State start;
-	start.map = {
-		{'g', 'g', 'g', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'g', '|', 'g', 'g', 'g', 'g', ' ', ' ', ' ', ' '},
-		{'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', ' '},
-		{' ', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g'},
-		{' ', ' ', ' ', ' ', ' ', 'g', 'g', '_', 'g', 'g'},
-		{' ', ' ', ' ', ' ', ' ', ' ', 'g', 'g', 'g', ' '}};
+	shared_ptr<State> start = analyzeMap();
 
-	vector<State> states;
+	vector<shared_ptr<State>> states;
 
 	states.push_back(start);
 	++nodes;
 
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
-	start = breadthFirstSearch(states);
+	shared_ptr<State> end = breadthFirstSearch(states);
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
-	auto duration = duration_cast<microseconds>( t2 - t1 ).count();
-    // cout << duration << endl;
+	auto duration = duration_cast<microseconds>(t2 - t1).count();
+	
+	cout << "Duration: " << (float) duration / 1000000 << " seconds.\n";
+	cout << "Nodes: " << nodes << ".\n";
 
-	cout << nodes << endl;
-		
-	displayMap(start.map);
+	displayMap(end);
 
 	return 0;
 }
